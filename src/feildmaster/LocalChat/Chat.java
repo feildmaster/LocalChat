bcbf75c3dbd768b280bdbc8e15ee97b1cbb27bf3
Feildmaster <admin@feildmaster.com>
2011-09-08 13:53:18 -0500

    v1.3

--- src/feildmaster/LocalChat/Chat.java
+++ src/feildmaster/LocalChat/Chat.java
@@ -1,23 +1,21 @@
 package feildmaster.LocalChat;
 
 import com.massivecraft.factions.Factions;
-import java.io.File;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
 
 public class Chat extends JavaPlugin {
     private int range = 1000;
     private String message = "No one within range of your voice...";
+    private String prefix = "!";
+    private String format = "%1$s shouts: %2$s";
     private Factions factions;
+    private Configuration config;
 
     public void onLoad() {
-        if(!(new File(getDataFolder(),"config.yml").exists())) {
-            getConfiguration().setProperty("range", range);
-            getConfiguration().setProperty("null_message", message);
-            getConfiguration().save();
-        }
-
+        config = getConfiguration();
         factions = (Factions)getServer().getPluginManager().getPlugin("Factions");
     }
 
@@ -27,20 +25,29 @@ public class Chat extends JavaPlugin {
     public void onEnable() {
         loadConfig();
 
-        getServer().getPluginManager().registerEvent(Type.PLAYER_CHAT, new ChatListener(this), Priority.Low, this);
+        getServer().getPluginManager().registerEvent(Type.PLAYER_CHAT, new ChatListener(this), Priority.Lowest, this);
 
         getServer().getLogger().info("[LocalChat] All chats are now ranged");
     }
 
     private void loadConfig() {
-        range = getConfiguration().getInt("range", range);
-        message = getConfiguration().getString("null_message", message);
+        config.load();
+        range = (int) Math.pow(config.getInt("local.range", range), 2);
+        message = config.getString("local.null_message", message);
+        prefix = config.getString("global.prefix", prefix);
+        format = config.getString("global.format", format);
+        config.save();
     }
 
     public int getRange() {
         return range;
     }
-
+    public String getPrefix() {
+        return prefix;
+    }
+    public String getFormat() {
+        return format;
+    }
     public String getMessage() {
         return message;
     }
--- src/feildmaster/LocalChat/ChatListener.java
+++ src/feildmaster/LocalChat/ChatListener.java
@@ -1,7 +1,6 @@
 package feildmaster.LocalChat;
 
 import java.util.HashSet;
-import java.util.Set;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerChatEvent;
@@ -16,40 +15,34 @@ public class ChatListener extends PlayerListener {
     }
 
     public void onPlayerChat(PlayerChatEvent event) {
-        Player s = event.getPlayer();
+        Player sender = event.getPlayer();
 
-        if(plugin.getFactionsPlugin() != null && plugin.getFactionsPlugin().isPlayerFactionChatting(s)) return;
+        if(plugin.getFactionsPlugin() != null && plugin.getFactionsPlugin().isPlayerFactionChatting(sender)) return;
 
-        range = plugin.getRange();
-        Set<Player> removed = new HashSet<Player>();
-
-        for(Player r : event.getRecipients()) {
-            if(s.getWorld() != r.getWorld() || outOfRange(s.getLocation(), r.getLocation()))
-                removed.add(r);
+        if(event.getMessage().startsWith(plugin.getPrefix())) {
+            event.setMessage(event.getMessage().substring(plugin.getPrefix().length()));
+            event.setFormat(plugin.getFormat());
+            return;
         }
 
-        if(!removed.isEmpty())
-            event.getRecipients().removeAll(removed);
+        range = plugin.getRange();
+
+        for(Player r : new HashSet<Player>(event.getRecipients()))
+            if(sender.getWorld() != r.getWorld() || outOfRange(sender.getLocation(), r.getLocation()))
+                event.getRecipients().remove(r);
 
         if(event.getRecipients().size() == 1) {
-            s.sendMessage(plugin.getMessage());
+            sender.sendMessage(plugin.getMessage());
             event.setCancelled(true);
         }
     }
 
-    /*
-     * Is this faster... or slower than
-     * location.distance(location)?
-     * Or should I just use
-     * location.distanceSquared, and Square the range?
-     */
     private Boolean outOfRange(Location l, Location ll) {
-        if(l == ll) return false;
-
-        if(Math.max(l.getBlockX(), ll.getBlockX())-Math.min(l.getBlockX(), ll.getBlockX())>range) return true;
-        if(Math.max(l.getBlockZ(), ll.getBlockZ())-Math.min(l.getBlockZ(), ll.getBlockZ())>range) return true;
-        if(Math.max(l.getBlockY(), ll.getBlockY())-Math.min(l.getBlockY(), ll.getBlockY())>range) return true;
-
+        if(l.equals(ll))
+            return false;
+        if(l.distanceSquared(ll)>range)
+            return true;
+        
         return false;
     }
 }
